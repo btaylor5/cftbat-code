3dd41512209dca5dd019f016fdb23942603edc0a
Bryant Taylor <btaylor5@uw.edu>
2016-09-29 20:50:03 -0700

    Completed chapter one. Wrote my first clojure program

---
+++ 01/my-clojure-noob/test/my_clojure_noob/core_test.clj
@@ -0,0 +1,7 @@
+(ns my-clojure-noob.core-test
+  (:require [clojure.test :refer :all]
+            [my-clojure-noob.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
